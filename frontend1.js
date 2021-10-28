f803c3221d1d632a2ab1d82f66f68ed294498716
akbarmaulana0204 <89144955+akbarmaulana0204@users.noreply.github.com>
2021-10-28 18:13:47 +0700

    Tugas frontend 1 GDSC ITB

---
+++ frontend1.js
@@ -0,0 +1,19 @@
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Rating: 5.8/10 <br> Director: M. Night Shyamalam <br> Description: A vacationing family discovers that the secluded beach where they're relaxing for a few hours is somehow causing them to age rapidly, reducing their entire lives into a single day. <br> ";
+}
+
+function myFunction2() {
+  document.getElementById("demo2").innerHTML = "Rating: 7.9/10 <br> Director: Destin Daniel Cretton <br> Description: Shang-Chi, the master of weaponry-based Kung Fu, is forced to confront his past after being drawn into the Ten Rings organization. <br> ";
+}
+
+function myFunction3() {
+  document.getElementById("demo3").innerHTML = "Rating: 7.6/10 <br> Director: Cary Joji Fukunaga <br> Description: James Bond has left active service. His peace is short-lived when Felix Leiter, an old friend from the CIA, turns up asking for help, leading Bond onto the trail of a mysterious villain armed with dangerous new technology. <br> ";
+}
+
+function myFunction4() {
+  document.getElementById("demo4").innerHTML = "Rating: 6.8/10 <br> Director: Cate Shortland <br> Description: Natasha Romanoff confronts the darker parts of her ledger when a dangerous conspiracy with ties to her past arises. <br> ";
+}
+
+function myFunction5() {
+  document.getElementById("demo5").innerHTML = "Rating: 8.3/10 <br> Director: Denis Vellenueve <br> Description: Feature adaptation of Frank Herbert's science fiction novel, about the son of a noble family entrusted with the protection of the most valuable asset and most vital element in the galaxy. <br> ";
+}
